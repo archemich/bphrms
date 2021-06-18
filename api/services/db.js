fc79060ac0642a507d68b0eee05b7b9a6e27235e
archemich <archemich@gmail.com>
2021-06-18 23:11:13 +0300

    sequilize connected

---
+++ api/services/db.js
@@ -0,0 +1,5 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  dialect: "mysql",
+  host: process.env.DB_HOST
+});
