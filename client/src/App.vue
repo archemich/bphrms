bd4a8e0b27eeb7bf8f14989077485056b0e88533
archemich <archemich@gmail.com>
2021-06-20 07:33:45 +0300

    vue import

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,45 +1,133 @@
 <template>
   <div id="wrap">
     <Header />
-    <h1>Авторизация</h1>
-    <hr>
-    <h2>Вход/Регистрация</h2>
-    <div class="row">
-      <button @click="isDoctorSet = true">Я врач</button>
-      <button @click="isDoctorSet = false">Я пациент</button>
-      </div>
-      <AuthForm @submitForm = "loginUser"/>
-      <p v-if="wrongPassword">Wrong login or password</p>
+      <main>
+    <div class="section">
+        <div class="container-980">
+            <!--            Табы -->
+            <div class="columns is-centered">
+                <div class="column is-8-desktop">
+                    <div class="box">
+                        <h2 class="title has-text-centered">Войти в систему</h2>
+                        <div class="tabs is-centered is-large is-fullwidth">
+                            <!--            https://bulma.io/documentation/components/tabs/    -->
+                            <ul>
+                                <li class="is-active"><a>Я пациент</a></li>
+                                <li class=""><a>Я врач</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <!--            Форма -->
+            <div class="columns is-centered">
+                <div class="column is-12-mobile is-8-desktop">
+                    <div class="box">
+                        <form @submit="(event)=>{event.preventDefault()}">
+                            <div class="field">
+                                <label class="label is-large" for="login">Логин</label>
+                                <div class="control has-icons-left has-icons-right">
+                                    <input class="input is-success is-large" type="text" placeholder="Text input"
+                                           v-model="login" id="login">
+                                    <span class="icon is-small is-left">
+      <i class="fas fa-user"></i>
+    </span>
+                                    <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+                                </div>
+                                <p class="help is-success">Логин свободен</p>
+                            </div>
+
+                            <div class="field">
+                                <label class="label is-large" for="password">Пароль</label>
+                                <div class="control has-icons-left has-icons-right">
+                                    <input class="input is-danger is-large" type="password" placeholder="Email input"
+                                          v-model="password" id="password">
+                                    <span class="icon is-small is-left">
+      <i class="fas fa-unlock-alt"></i>
+    </span>
+                                    <span class="icon is-small is-right">
+      <i class="fas fa-exclamation-triangle"></i>
+    </span>
+                                </div>
+                                <!--                        <p class="help is-danger">Па</p>-->
+                            </div>
+
+                            <div class="field">
+                                <div class="control ">
+                                    <label class="checkbox is-size-4 ">
+                                        <input type="checkbox">
+                                        Я согласен с условиями использования и ознакомлен с <a href="#">законом о защите
+                                        персональных данных</a>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="field is-grouped is-grouped-centered my-6">
+                                <div class="control">
+                                    <button @click.prevent="loginUser" class="button is-success is-large">Войти</button>
+                                </div>
+                            </div>
+                            <hr class="mt-6">
+                            
+                            <h2 class="subtitle has-text-centered my-6 is-size-3">ИЛИ</h2>
+
+                            <div class="field is-grouped is-grouped-centered my-6">
+                                <div class="control">
+                                    <button class="button is-link is-large">Войти через <span
+                                            class="has-text-weight-semibold ml-2">ГосУслуги</span></button>
+                                </div>
+                            </div>
+
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+</main>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
-import AuthForm from "./components/AuthForm.vue";
 import axios from "axios";
+import VueRouter from 'vue-router';
 
 export default {
   name: "App",
   components: {
     Header,
-    AuthForm
   },
+
   data() {
     return {
       user: null,
       isDoctorSet: true,
-      wrongPassword: false
+      wrongPassword: false,
+      login: null,
+      password: null
     };
   },
 
   methods: {
-      loginUser(event) {
+      loginUser() {
       let role = this.isDoctorSet ? "doctor" : "patient";
-      let login = event.login;
-      let password = event.password;
-      axios.post(process.env.VUE_APP_API_URL + "auth/login", {login,password,role})
+      let login = this.login;
+      let password = this.password;
+      console.log(login);
+      axios.post( "https://bphrmsapi.azurewebsites.net/auth/login", {login,password,role})
       .then ((res) => {
         console.log(res);
+        this.$router.push('/');
+         
+
         })
       .catch(() => {
         this.wrongPassword = true;
@@ -65,18 +153,7 @@ export default {
 </script>
 
 <style>
-html,
-body {
-  margin: 0;
-  padding: 0;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 0;
-  padding: 0;
-}
+@import '../../front/bulma/css/bulma.css';
+@import '../../front/css/style.css';
+
 </style>
--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,21 +1,59 @@
 <template>
-  <header class="header">
-    <h2 v-if="username" class="username">{{ username }}</h2>
-  </header>
+  <header class="">
+    <nav class="navbar mb-5 p-4" role="navigation" aria-label="main navigation">
+        <div class="container">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="https://bulma.io">
+                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+                </a>
+
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
+                   data-target="navbarBasicExample">
+                    <div class="menu-icon is-large">
+                        <i class="fas fa-bars is-size-4"></i>
+                    </div>
+                </a>
+
+            </div>
+            <div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-start">
+
+                </div>
+                <div class="navbar-end is-large">
+                    <div class="navbar-item has-dropdown is-hoverable">
+
+                        <a class="navbar-link">
+                            Меню
+                        </a>
+
+                        <div class="navbar-dropdown is-mobile ">
+                            <a class="navbar-item">
+                                About
+                            </a>
+                            <a class="navbar-item">
+                                Jobs
+                            </a>
+
+                            <hr class="navbar-divider">
+                            <a class="navbar-item">
+                                Выход
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+</header>
 </template>
 
 <script>
 export default {
   name: "Header",
   props: ["username"],
-  methods: {
-    openNav() {
-      document.getElementById("mySidenav").style.width = "250px";
-    },
-    closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-    },
-  },
+  
 };
 </script>
 <style scoped>
